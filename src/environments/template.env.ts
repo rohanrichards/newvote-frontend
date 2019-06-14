f9b87c34438a8eae81865f6695eee6fd846c42f1
Rohan Richards <rohan.m.richards@gmail.com>
2019-06-14 12:06:56 +1000

    pull recaptcha key into .env file

---
+++ src/environments/template.env.ts
@@ -0,0 +1,7 @@
+// copy this file and name it .env.ts and make sure to not commit it to git
+// fill in the required environment variables
+
+export default {
+	'npm_package_version': '1.0.0',
+	'recaptchaSitekey': 'RECAPTCHA_SITE_KEY'
+};
