da8296162fac4b449d265de952f06f06afbfbe30
Jamie <jamieplayer@gmail.com>
2019-07-15 19:39:25 +0100

    error placeholder card

---
+++ src/app/shared/error/error-card/error-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-error-card',
+  templateUrl: './error-card.component.html',
+  styleUrls: ['./error-card.component.scss']
+})
+export class ErrorCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
