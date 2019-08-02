487d3efe31ff68ce06b9dc084b36efe375103ddb
Jamie <jamieplayer@gmail.com>
2019-08-02 21:27:38 +0100

    updates for the shell + notes on tracking scroll position

--- src/app/shell/shell.component.ts
+++ src/app/shell/shell.component.ts
@@ -1,13 +1,24 @@
 import { Title } from '@angular/platform-browser';
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit, Input, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
+import { Event, NavigationEnd, NavigationStart, Router } from '@angular/router';
 import { ObservableMedia } from '@angular/flex-layout';
 import { CookieService } from 'ngx-cookie-service';
 
 import { AuthenticationService, I18nService } from '@app/core';
 import { OrganizationService } from '@app/core/http/organization/organization.service';
 import { MetaService } from '@app/core/meta.service';
+import { MatSidenavContent } from '@angular/material';
 
+import { asyncScheduler, fromEvent } from 'rxjs';
+import { filter, observeOn, scan, debounceTime, tap } from 'rxjs/operators';
+import { CdkScrollable, ScrollDispatcher } from '@angular/cdk/overlay';
+
+interface ScrollPositionRestore {
+	event: Event;
+	positions: { [K: number]: number };
+	trigger: 'imperative' | 'popstate';
+	idToRestore: number;
+}
 
 @Component({
 	selector: 'app-shell',
@@ -20,8 +31,13 @@ export class ShellComponent implements OnInit {
 	showSearch = false;
 
 	@Output() closeSearch = new EventEmitter();
+	@ViewChild(CdkScrollable) scrollable: CdkScrollable;
 
-	constructor(private router: Router,
+	scrollingSubscription: any;
+
+	constructor(
+		public scroll: ScrollDispatcher,
+		private router: Router,
 		private titleService: Title,
 		private media: ObservableMedia,
 		private auth: AuthenticationService,
@@ -29,7 +45,25 @@ export class ShellComponent implements OnInit {
 		private organizationService: OrganizationService,
 		private meta: MetaService,
 		private cookieService: CookieService
-	) { }
+	) { 
+		// NOTES
+		// unless mat-sidenav-container / children have a fixed height i.e 100vh
+		// the height property is passed all the way to the root app
+		// at root we can't get scroll info
+		// using CDK scrollable + container 'vh100' we can listen into scroll events on the content
+		// need to find a way of tracking the page, the scroll position (scrollTop on data.elementRef)
+		// saving that data and then triggering on page forward a scroll reset to 0
+		// or if back is pushed the scrollposition is then applied after view init
+		this.scrollingSubscription = this.scroll
+		.scrolled()
+		.subscribe((data: CdkScrollable) => {
+			let element = data.elementRef;
+			console.log(element, 'this is element');
+
+			
+		});
+	}
+
 
 	ngOnInit() {
 		this.organizationService.get().subscribe(org => {
