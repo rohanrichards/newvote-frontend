77886d4d866f09057813090ee943f303f3d61e24
Rohan Richards <rohan.m.richards@gmail.com>
2019-08-26 12:02:42 +1000

    update packages
    removed cordova imports completely as they are not used
    updated ng-recaptcha to latest version
    rolled back typescript version as Angular6 requires <3.3.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,6 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 import { TranslateModule } from '@ngx-translate/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { Angulartics2Module } from 'angulartics2';
 import { Angulartics2GoogleAnalytics } from 'angulartics2/ga';
 import { SwiperModule } from 'ngx-swiper-wrapper';
@@ -79,8 +77,6 @@ import { ScrollingModule } from '@angular/cdk/scrolling';
 		ConfirmDialogComponent
 	],
 	providers: [
-		StatusBar,
-		SplashScreen,
 		CookieService,
 		{
 			provide: RECAPTCHA_SETTINGS,
