ce2f5a89e620f9040ae085e8141cbadbed084f48
Jamie Player <jamieplayer@gmail.com>
2019-09-11 21:09:38 +0100

    mapped proposal store to solutions

--- src/app/core/http/solution/solution.query.ts
+++ src/app/core/http/solution/solution.query.ts
@@ -1,11 +1,45 @@
 import { Injectable } from "@angular/core";
-import { QueryEntity } from "@datorama/akita";
+import { QueryEntity, combineQueries } from "@datorama/akita";
 import { SolutionState, SolutionStore } from "./solution.state";
 import { Solution } from "@app/core/models/solution.model";
+import { combineLatest } from "rxjs";
+import { ProposalQuery } from "../proposal/proposal.query";
+import { auditTime, map } from "rxjs/operators";
+import { IssueQuery } from "../issue/issue.query";
+import { VotesQuery } from "../vote/vote.query";
 
 @Injectable()
 export class SolutionQuery extends  QueryEntity<SolutionState, Solution> {
-    constructor(protected store: SolutionStore) {
+    constructor(
+        protected store: SolutionStore,
+        private proposalQuery: ProposalQuery,
+        private issueQuery: IssueQuery,
+        private voteQuery: VotesQuery
+    ) {
         super(store);
+    }
+
+    selectSolutions() {
+        return combineQueries(
+            [
+                this.selectAll(),
+                this.proposalQuery.selectAll({ asObject: true }),
+            ]
+        )
+            .pipe(
+                map((results) => {
+                    const [solutions, proposals] = results;
+
+                    return solutions.map((solution) => {
+                        return {
+                            ...solution,
+                            proposals: solution.proposals.map((proposalId) => {
+                                return proposals[proposalId]
+                            }),
+                            votes: solution.votes
+                        }
+                    })
+                })
+            )
     }
 }
