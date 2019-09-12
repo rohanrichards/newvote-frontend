0aa4e0d92ccc80864e5d8c4c7474101c149744a5
Jamie Player <jamieplayer@gmail.com>
2019-09-12 11:58:55 +0100

    admin command service

---
+++ src/app/core/http/admin/admin.service.ts
@@ -0,0 +1,115 @@
+import { Injectable } from '@angular/core';
+import { SuggestionService } from '../suggestion/suggestion.service';
+import { SolutionService } from '../solution/solution.service';
+import { IssueService } from '../issue/issue.service';
+import { ProposalService } from '../proposal/proposal.service';
+import { MediaService } from '../media/media.service';
+import { OrganizationService } from '../organization/organization.service';
+import { assign } from 'lodash';
+import { MatDialog, MatDialogRef, MatSnackBar } from '@angular/material';
+import { ConfirmDialogComponent } from '@app/shared/confirm-dialog/confirm-dialog.component';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class AdminService {
+
+	entities: ['Issue', 'Solution', 'Proposal', 'Suggestion', 'Media', 'Organization'];
+
+	constructor(
+		private organizationService: OrganizationService,
+		private issueService: IssueService,
+		private solutionService: SolutionService,
+		private suggestionService: SuggestionService,
+		private proposalService: ProposalService,
+		private mediaService: MediaService,
+		private dialog: MatDialog,
+		private snackBar: MatSnackBar
+	) { }
+	
+	getService(model: string) {
+		const services = [this.issueService, this.solutionService, this.proposalService, this.suggestionService, this.mediaService, this.organizationService]
+		let entityIndex = this.entities.findIndex(e => e === model);
+		return services[entityIndex];
+	}
+
+	onDelete(object: any, model: string) {
+		const title = object.name || object.title;
+		const service = this.getService(model);
+
+		const dialogRef: MatDialogRef<ConfirmDialogComponent> = this.dialog.open(ConfirmDialogComponent, {
+			width: '250px',
+			data: {
+				title: `Delete ${model}?`,
+				message: `Are you sure you want to delete ${title}? This action cannot be undone.`
+			}
+		});
+
+		dialogRef.afterClosed().subscribe((confirm: boolean) => {
+			if (confirm) {
+				service.delete({ id: object._id })
+					.subscribe(() => {
+						this.openSnackBar('Succesfully deleted', 'OK');
+						// this.router.navigate(['/issues'], { queryParams: { forceUpdate: true } });
+					});
+			}
+		});
+
+	}
+
+	onSoftDelete(object: any, model: string) {
+		const title = object.name || object.title;
+		const service = this.getService(model);
+		const entity = assign({}, object, { softDeleted: false });
+
+		const dialogRef: MatDialogRef<ConfirmDialogComponent> = this.dialog.open(ConfirmDialogComponent, {
+			width: '250px',
+			data: {
+				title: `Remove ${title}?`,
+				message: `Are you sure you want to remove ${title}? This will only hide the item from the public.`
+			}
+		});
+
+		dialogRef.afterClosed().subscribe((confirm: boolean) => {
+			if (confirm) {
+				return service.update({ id: entity._id, entity })
+					.subscribe(() => {
+						this.openSnackBar('Succesfully removed', 'OK');
+					});
+			}
+		});
+		
+	}
+
+	onRestore(object: any, model: string) {
+		const title = object.name || object.title;
+		const service = this.getService(model);
+		const entity = assign({}, object, { softDeleted: false });
+
+		const dialogRef: MatDialogRef<ConfirmDialogComponent> = this.dialog.open(ConfirmDialogComponent, {
+			width: '250px',
+			data: {
+				title: `Restore ${title}?`,
+				message: `Are you sure you want to restore ${title}? This will make the item visible to the public.`
+			}
+		});
+
+		dialogRef.afterClosed().subscribe((confirm: boolean) => {
+			if (confirm) {	
+				return service.update({ id: entity._id, entity })
+					.subscribe(() => {
+						this.openSnackBar('Succesfully restored', 'OK');
+						// this.router.navigate(['/issues'], { queryParams: { forceUpdate: true } });
+					});
+			}
+		});
+	}
+
+	openSnackBar(message: string, action: string) {
+		this.snackBar.open(message, action, {
+			duration: 4000,
+			horizontalPosition: 'right',
+			verticalPosition: 'bottom',
+		});
+	}
+}
