ddede382ac5a0b7bab0cbcdb3164255b622fa388
Jamie Player <jamieplayer@gmail.com>
2019-09-25 12:28:21 +0100

    commented out cache on http requests

--- src/app/core/http/proposal/proposal.service.ts
+++ src/app/core/http/proposal/proposal.service.ts
@@ -9,104 +9,104 @@ import { VoteService } from '../vote/vote.service';
 import { ProposalStore } from './proposal.store';
 
 const routes = {
-	list: (c: ProposalContext) => `/proposals`,
-	view: (c: ProposalContext) => `/proposals/${c.id}`,
-	create: (c: ProposalContext) => `/proposals`,
-	update: (c: ProposalContext) => `/proposals/${c.id}`,
-	delete: (c: ProposalContext) => `/proposals/${c.id}`
+    list: (c: ProposalContext) => `/proposals`,
+    view: (c: ProposalContext) => `/proposals/${c.id}`,
+    create: (c: ProposalContext) => `/proposals`,
+    update: (c: ProposalContext) => `/proposals/${c.id}`,
+    delete: (c: ProposalContext) => `/proposals/${c.id}`
 };
 
 export interface ProposalContext {
-	// The proposal's category: 'dev', 'explicit'...
-	orgs?: Array<string>; // users organisations
-	id?: string; // id of object to find/modify
-	entity?: IProposal; // the object being created or edited
-	params?: any;
-	forceUpdate?: boolean;
+    // The proposal's category: 'dev', 'explicit'...
+    orgs?: Array<string>; // users organisations
+    id?: string; // id of object to find/modify
+    entity?: IProposal; // the object being created or edited
+    params?: any;
+    forceUpdate?: boolean;
 }
 
 @Injectable()
 export class ProposalService {
 
-	constructor(
-		private voteService: VoteService,
-		private httpClient: HttpClient,
-		private proposalStore: ProposalStore
-	) { }
+    constructor(
+        private voteService: VoteService,
+        private httpClient: HttpClient,
+        private proposalStore: ProposalStore
+    ) { }
 
-	list(context: ProposalContext): Observable<any[]> {
-		// create blank params object
-		let params = new HttpParams();
+    list(context: ProposalContext): Observable<any[]> {
+        // create blank params object
+        let params = new HttpParams();
 
-		// if we have params provided in the context, replace with those
-		if (context.params) {
-			// context.params is assumed to have a format similar to
-			// { topicId: [id], search: [search terms], ...}
-			params = new HttpParams({ fromObject: context.params });
-		}
+        // if we have params provided in the context, replace with those
+        if (context.params) {
+            // context.params is assumed to have a format similar to
+            // { topicId: [id], search: [search terms], ...}
+            params = new HttpParams({ fromObject: context.params });
+        }
 
-		return this.httpClient
-			.cache(context.forceUpdate)
-			.get(routes.list(context), { params })
-			.pipe(
-				tap((data: Proposal[]) => {
-					this.voteService.populateStore(data);
-					this.proposalStore.add(data);
-				}),
-				map((res: Array<any>) => res),
-				catchError(handleError)
-			);
-	}
+        return this.httpClient
+            // .cache(context.forceUpdate)
+            .get(routes.list(context), { params })
+            .pipe(
+                tap((data: Proposal[]) => {
+                    this.voteService.populateStore(data);
+                    this.proposalStore.add(data);
+                }),
+                map((res: Array<any>) => res),
+                catchError(handleError)
+            );
+    }
 
-	view(context: ProposalContext): Observable<any> {
-		return this.httpClient
-			.cache(context.forceUpdate)
-			.get(routes.view(context))
-			.pipe(
-				tap((res: Proposal) => {
-					this.voteService.addEntityVote(res);
-					this.proposalStore.add(res);
-				}),
-				map((res: any) => res),
-				catchError(handleError)
-			);
-	}
+    view(context: ProposalContext): Observable<any> {
+        return this.httpClient
+            // .cache(context.forceUpdate)
+            .get(routes.view(context))
+            .pipe(
+                tap((res: Proposal) => {
+                    this.voteService.addEntityVote(res);
+                    this.proposalStore.add(res);
+                }),
+                map((res: any) => res),
+                catchError(handleError)
+            );
+    }
 
-	create(context: ProposalContext): Observable<any> {
-		return this.httpClient
-			.cache(context.forceUpdate)
-			.post(routes.create(context), context.entity)
-			.pipe(
-				tap((res: Proposal) => this.proposalStore.add(res)),
-				map((res: any) => res),
-				catchError(handleError)
-			);
-	}
+    create(context: ProposalContext): Observable<any> {
+        return this.httpClient
+            // .cache(context.forceUpdate)
+            .post(routes.create(context), context.entity)
+            .pipe(
+                tap((res: Proposal) => this.proposalStore.add(res)),
+                map((res: any) => res),
+                catchError(handleError)
+            );
+    }
 
-	update(context: ProposalContext): Observable<any> {
-		return this.httpClient
-			.cache(context.forceUpdate)
-			.put(routes.update(context), context.entity)
-			.pipe(
-				tap((proposal: Proposal) => this.proposalStore.upsert(proposal._id, proposal)),
-				map((res: any) => res),
-				catchError(handleError)
-			);
-	}
+    update(context: ProposalContext): Observable<any> {
+        return this.httpClient
+            // .cache(context.forceUpdate)
+            .put(routes.update(context), context.entity)
+            .pipe(
+                tap((proposal: Proposal) => this.proposalStore.upsert(proposal._id, proposal)),
+                map((res: any) => res),
+                catchError(handleError)
+            );
+    }
 
-	delete(context: ProposalContext): Observable<any> {
-		return this.httpClient
-			.cache(context.forceUpdate)
-			.delete(routes.delete(context))
-			.pipe(
-				tap((proposal: Proposal) => this.proposalStore.remove(proposal._id)),
-				map((res: any) => res),
-				catchError(handleError)
-			);
-	}
+    delete(context: ProposalContext): Observable<any> {
+        return this.httpClient
+            // .cache(context.forceUpdate)
+            .delete(routes.delete(context))
+            .pipe(
+                tap((proposal: Proposal) => this.proposalStore.remove(proposal._id)),
+                map((res: any) => res),
+                catchError(handleError)
+            );
+    }
 
-	updateProposalVote(id: string, proposal: any) {
-		this.proposalStore.update(id, proposal);
-	}
+    updateProposalVote(id: string, proposal: any) {
+        this.proposalStore.update(id, proposal);
+    }
 
 }
--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -8,80 +8,80 @@ import { MatSidenav } from '@angular/material';
 import { MediaObserver } from '@angular/flex-layout';
 
 @Component({
-	selector: 'div[sticky-component]',
-	templateUrl: './navbar.component.html',
-	styleUrls: ['./navbar.component.scss']
+    selector: 'div[sticky-component]',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
 
-	@Input() organization: Organization;
-	@Input() sideNavRef: MatSidenav;
+    @Input() organization: Organization;
+    @Input() sideNavRef: MatSidenav;
 
-	showSearch = false;
-	hideVerify = false;
+    showSearch = false;
+    hideVerify = false;
 
-	constructor(
-		private meta: MetaService,
-		private titleService: Title,
-		public auth: AuthenticationService,
-		private router: Router,
-		private media: MediaObserver,
-	) { }
+    constructor(
+        private meta: MetaService,
+        private titleService: Title,
+        public auth: AuthenticationService,
+        private router: Router,
+        private media: MediaObserver,
+    ) { }
 
-	ngOnInit() {
-	}
+    ngOnInit() {
+    }
 
-	toggleSearch() {
-		this.showSearch = !this.showSearch;
-	}
+    toggleSearch() {
+        this.showSearch = !this.showSearch;
+    }
 
-	logout() {
-		this.auth.logout()
-			.subscribe(() => this.router.navigate(['/login'], { replaceUrl: true }));
-	}
+    logout() {
+        this.auth.logout()
+            .subscribe(() => this.router.navigate(['/'], { replaceUrl: true }));
+    }
 
-	get title(): string {
-		return this.meta.getAppBarTitle();
-	}
+    get title(): string {
+        return this.meta.getAppBarTitle();
+    }
 
-	get isAuthenticated(): boolean {
-		return this.auth.isAuthenticated();
-	}
+    get isAuthenticated(): boolean {
+        return this.auth.isAuthenticated();
+    }
 
-	get isVerified(): boolean {
-		// debugger;
-		if (this.isAuthenticated) {
-			return (this.auth.isVerified());
-		} else {
-			return true;
-		}
-	}
+    get isVerified(): boolean {
+        // debugger;
+        if (this.isAuthenticated) {
+            return (this.auth.isVerified());
+        } else {
+            return true;
+        }
+    }
 
-	get showVerify(): boolean {
-		if (this.isVerified) {
-			return false;
-		}
-		if (!this.isVerified && !this.hideVerify) {
-			return true;
-		} else {
-			return false;
-		}
-	}
+    get showVerify(): boolean {
+        if (this.isVerified) {
+            return false;
+        }
+        if (!this.isVerified && !this.hideVerify) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-	get isCommunityVerified(): boolean {
-		return this.auth.isCommunityVerified();
-	}
+    get isCommunityVerified(): boolean {
+        return this.auth.isCommunityVerified();
+    }
 
-	get isMobile(): boolean {
-		return this.media.isActive('xs') || this.media.isActive('sm');
-	}
+    get isMobile(): boolean {
+        return this.media.isActive('xs') || this.media.isActive('sm');
+    }
 
-	get username(): string | null {
-		const credentials = this.auth.credentials;
-		return credentials ? credentials.user.username : null;
-	}
+    get username(): string | null {
+        const credentials = this.auth.credentials;
+        return credentials ? credentials.user.username : null;
+    }
 
-	handleToggle()  {
-		this.sideNavRef.toggle();
-	}
+    handleToggle() {
+        this.sideNavRef.toggle();
+    }
 }
