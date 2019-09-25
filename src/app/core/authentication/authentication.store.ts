9baa24c24fdc8625e350b13bf2b4764c78d6ce4a
Jamie Player <jamieplayer@gmail.com>
2019-09-25 09:25:42 +0100

    created auth store

---
+++ src/app/core/authentication/authentication.store.ts
@@ -0,0 +1,28 @@
+import { Injectable } from "@angular/core";
+import { StoreConfig, Store } from "@datorama/akita";
+import { IUser } from "../models/user.model";
+
+export function createInitialState(): IUser {
+    return {
+        _id: '',
+        firstName: '',
+        lastName: '',
+        displayName: '',
+        email: '',
+        postalCode: '',
+        username: '',
+        mobileNumber: '',
+        verified: false,
+        profileImageURL: '',
+        roles: [],
+        completedTour: false
+    }
+}
+
+@Injectable({ providedIn: 'root' })
+@StoreConfig({ name: 'auth', resettable: true })
+export class AuthStore extends Store<IUser> {
+    constructor() {
+        super(createInitialState());
+    }
+}
