f2631b15489ed54de2e4d1b0d0567d4eff84ab08
Jamie Player <jamieplayer@gmail.com>
2019-10-03 10:19:33 +0100

    updated models to have slug property

--- src/app/core/models/issue.model.ts
+++ src/app/core/models/issue.model.ts
@@ -1,26 +1,28 @@
 export interface IIssue {
-	_id?: string;
-	name: string;
-	imageUrl: string;
-	description: string;
-	user?: any;
-	solutionMetaData?: any;
-	topics?: Array<any>;
-	created?: Date;
-	organizations: any;
-	softDeleted: boolean;
-	mediaHeading?: string;
-	suggestionTemplate?: string;
+    _id?: string;
+    name: string;
+    imageUrl: string;
+    description: string;
+    user?: any;
+    solutionMetaData?: any;
+    topics?: Array<any>;
+    created?: Date;
+    organizations: any;
+    softDeleted: boolean;
+    mediaHeading?: string;
+    suggestionTemplate?: string;
+    slug: string;
 }
 
 export class Issue implements IIssue {
-	public constructor(
-		public _id: string = '',
-		public name: string = '',
-		public imageUrl: string = '',
-		public description: string = '',
-		public organizations: any = {},
-		public topics: Array<any> = [],
-		public softDeleted: boolean = false
-	) { }
+    public constructor(
+        public _id: string = '',
+        public name: string = '',
+        public imageUrl: string = '',
+        public description: string = '',
+        public organizations: any = {},
+        public topics: Array<any> = [],
+        public softDeleted: boolean = false,
+        public slug: string = '',
+    ) { }
 }
