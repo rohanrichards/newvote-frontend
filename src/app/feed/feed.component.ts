169fc41a5d11b663c0a4673a4779c1a205d05789
Jamie Player <jamieplayer@gmail.com>
2019-10-31 16:59:48 +0000

    updated output on progress-form, fixed margin issue on feed

--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core'
+import { FeedService } from '@app/core/http/feed'
 
 @Component({
     selector: 'app-feed',
@@ -7,12 +8,13 @@ import { Component, OnInit } from '@angular/core'
 })
 export class FeedComponent implements OnInit {
 
-    constructor() { }
+    constructor(private feedService: FeedService) { }
 
     ngOnInit() {
     }
 
     updateProgress(state: string) {
+        console.log(state, 'this is state')
         // this.feedService.update(state);
     }
 }
