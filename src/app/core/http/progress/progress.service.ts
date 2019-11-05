16fe0fd814231c3d1de381b06ba480fa16dc7758
Jamie Player <jamieplayer@gmail.com>
2019-11-05 16:32:32 +0000

    progress service api calls

--- src/app/core/http/progress/progress.service.ts
+++ src/app/core/http/progress/progress.service.ts
@@ -1,7 +1,24 @@
 import { Injectable } from '@angular/core'
 import { ProgressStore } from './progress.store'
-import { HttpClient } from '@angular/common/http'
-import { tap } from 'rxjs/operators'
+import { HttpClient, HttpParams } from '@angular/common/http'
+import { tap, catchError } from 'rxjs/operators'
+import { IProgress, Progress } from '@app/core/models/progress.model'
+import { Observable } from 'rxjs'
+import { handleError } from '../errors'
+
+export interface ProgressContext {
+    id?: string;
+    entity?: IProgress;
+    params?: any;
+}
+
+const routes = {
+    create: () => '/progress',
+    list: () => '/progress',
+    view: (c: ProgressContext) => `/progress/${c.id}`,
+    update: (c: ProgressContext) => `/progress/${c.id}`,
+    delete: (c: ProgressContext) => `/progress/${c.id}`,
+}
 
 @Injectable({ providedIn: 'root' })
 export class ProgressService {
@@ -10,7 +27,58 @@ export class ProgressService {
         private http: HttpClient) {
     }
 
-    // get() {
-    //     return this.http.get('').pipe(tap(entities => this.progressStore.set(entities)))
-    // }
+    get(context: ProgressContext): Observable<any> {
+        let params = new HttpParams()
+
+        if (context.params) {
+            params = new HttpParams({ fromObject: context.params })
+        }
+
+        const options = {
+            withCredentials: true,
+            params
+        }
+
+        return this.http
+            .get(routes.list(), options)
+            .pipe(
+                tap(entities => this.progressStore.set(entities))
+            )
+    }
+
+    create(context: ProgressContext): Observable<Progress> {
+        return this.http
+            .post(routes.create(), context.entity)
+            .pipe(
+                catchError(handleError),
+                tap((progress: Progress) => this.progressStore.add(progress))
+            )
+    }
+
+    view(context: ProgressContext): Observable<Progress> {
+        return this.http
+            .get(routes.view(context))
+            .pipe(
+                catchError(handleError),
+                tap((progress: Progress) => this.progressStore.add(progress))
+            )
+    }
+
+    update(context: ProgressContext): Observable<Progress> {
+        return this.http
+            .put(routes.update(context), context.entity)
+            .pipe(
+                catchError(handleError),
+                tap((progress: Progress) => this.progressStore.upsert(progress._id, progress))
+            )
+    }
+
+    delete(context: ProgressContext): Observable<Progress> {
+        return this.http
+            .delete(routes.delete(context))
+            .pipe(
+                catchError(handleError),
+                tap((progress: Progress) => this.progressStore.remove(progress._id))
+            )
+    }
 }
