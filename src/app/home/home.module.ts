7dc1357619fc8fe6840bfe61ece793dd1a6e57b3
Jamie <jamieplayer@gmail.com>
2020-04-16 17:20:16 +0100

    imported router and updated routerLink call

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,32 +4,28 @@ import { TranslateModule } from '@ngx-translate/core'
 import { FlexLayoutModule } from '@angular/flex-layout'
 import { Angulartics2Module } from 'angulartics2'
 
-import { CoreModule } from '@app/core'
 import { SharedModule } from '@app/shared'
 import { MaterialModule } from '@app/material.module'
 import { HomeRoutingModule } from './home-routing.module'
 import { HomeComponent } from './home.component'
-import { IssueService } from '@app/core/http/issue/issue.service'
-import { SolutionService } from '@app/core/http/solution/solution.service'
-import { ProposalService } from '@app/core/http/proposal/proposal.service'
-import { UserService } from '@app/core/http/user/user.service'
 
 import { LazyLoadImageModule, intersectionObserverPreset } from 'ng-lazyload-image'
 import { JoyrideModule } from 'ngx-joyride';
 import { HomeInfoPanelComponent } from './components/home-info-panel/home-info-panel.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { IssuesPanelComponent } from './components/issues-panel/issues-panel.component'
+import { RouterModule } from '@angular/router'
 
 @NgModule({
     imports: [
         CommonModule,
         TranslateModule,
-        CoreModule,
         SharedModule,
         FlexLayoutModule,
         MaterialModule,
         Angulartics2Module,
         HomeRoutingModule,
+        RouterModule,
         JoyrideModule.forChild(),
         LazyLoadImageModule.forRoot({
             preset: intersectionObserverPreset
@@ -42,10 +38,6 @@ import { IssuesPanelComponent } from './components/issues-panel/issues-panel.com
         IssuesPanelComponent,
     ],
     providers: [
-        IssueService,
-        ProposalService,
-        UserService,
-        SolutionService
     ]
 })
 export class HomeModule { }
