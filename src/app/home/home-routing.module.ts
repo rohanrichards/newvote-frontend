ad2fd3d869bb668cf6e75002286937833990f164
Jamie <jamieplayer@gmail.com>
2020-04-16 16:27:22 +0100

    moved entity routes from app-routing module

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -3,13 +3,39 @@ import { Routes, RouterModule } from '@angular/router'
 
 import { extract } from '@app/core'
 import { HomeComponent } from './home.component'
-import { Shell } from '@app/shell/shell.service'
 
 const routes: Routes = [
-    Shell.childRoutes([
-        { path: 'home', redirectTo: '', pathMatch: 'full' },
-        { path: '', component: HomeComponent, data: { title: extract('NewVote') } }
-    ])
+    {
+        path: ':id',
+        children: [
+            {
+                path: '',
+                component: HomeComponent,
+                data: {
+                    title: extract('NewVote')
+                } 
+            },
+            {
+                path: 'topics', loadChildren: 'app/topic/topic.module#TopicModule'
+            },
+            {
+                path: 'issues', loadChildren: 'app/issue/issue.module#IssueModule'
+            },
+            {
+                path: 'solutions', loadChildren: 'app/solution/solution.module#SolutionModule'
+            },
+            {
+                path: 'suggestions', loadChildren: 'app/suggestion/suggestion.module#SuggestionModule'
+            },
+            {
+                path: 'media', loadChildren: 'app/media/media.module#MediaModule'
+            },
+            {
+                path: 'reps', loadChildren: 'app/reps/reps.module#RepsModule'
+            }
+        ]
+    },
+    { path: '', redirectTo: '/communities', pathMatch: 'full' },
 ]
 
 @NgModule({
